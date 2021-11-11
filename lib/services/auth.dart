e15b6854bcaf13087ddbf18d27d060df0b86e7ab
chandra-deb <mail.chandradeb@gmail.com>
2021-11-11 13:20:31 -0500

    add email sign in and create account code

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:time_tracker/models/user_client.dart';
@@ -8,6 +9,10 @@ abstract class AuthBase {
   UserClient? currentUser();
   Future<UserClient?> signInAnonymously();
   Future<UserClient?> signInWithGoogle();
+  Future<UserClient?> signInWithEmailAndPassword(String email, String password);
+  Future<UserClient?> createAccountWithEmailAndPassword(
+      String email, String password);
+
   Future<void> signOut();
 }
 
@@ -64,6 +69,22 @@ class Auth implements AuthBase {
         message: 'Sign in aborted by user',
       );
     }
+  }
+
+  @override
+  Future<UserClient?> signInWithEmailAndPassword(
+      String email, String password) async {
+    UserCredential userCredential = await _firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password);
+    return _userFromFirebase(userCredential.user);
+  }
+
+  @override
+  Future<UserClient?> createAccountWithEmailAndPassword(
+      String email, String password) async {
+    UserCredential userCredential = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return _userFromFirebase(userCredential.user);
   }
 
   @override
